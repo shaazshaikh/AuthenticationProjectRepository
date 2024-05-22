d42ee13f1e17c4e458e784ae62168af95ee30089
Shaaz Shaikh <shaazshaikh111@gmail.com>
2024-05-23 04:17:38 +0530

    Testing purpose 2

--- AuthenticationProject/WebApi/Controller/LoginController.cs
+++ AuthenticationProject/WebApi/Controller/LoginController.cs
@@ -24,7 +24,7 @@ namespace AuthenticationProject.WebApi.Controller
             var user = await _userLoginRepository.AuthenticateUserAsync(model.UserName, model.Password);
             if(user != null)
             {
-                //var token = GenerateToken(user); //Testing purpose
+                //var token = GenerateToken(user); //Testing purpose 2
                 return Ok();
             }
 
