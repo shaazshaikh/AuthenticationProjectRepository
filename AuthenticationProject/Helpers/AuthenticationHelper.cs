bd69a046da356f4ce8129641a4c2f76e0efb8913
shaazshaikh <shaazshaikh111@gmail.com>
2024-08-16 01:35:10 +0530

    generating token with claim as userId instead of userName

--- AuthenticationProject/Helpers/AuthenticationHelper.cs
+++ AuthenticationProject/Helpers/AuthenticationHelper.cs
@@ -18,7 +18,7 @@ namespace AuthenticationProject.Helpers
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
             var claims = new[]
-            { new Claim(ClaimTypes.NameIdentifier, user.UserName) };
+            { new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()) };
             var token = new JwtSecurityToken(Configuration["Jwt:Issuer"], Configuration["Jwt:Audience"], claims, signingCredentials:  credentials, expires: DateTime.Now.AddMinutes(20));
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
