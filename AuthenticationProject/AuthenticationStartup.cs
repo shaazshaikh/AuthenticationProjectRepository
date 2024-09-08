bf9c965e988f1bd6b945356e08e3f5ee137133e4
shaazshaikh <shaazshaikh111@gmail.com>
2024-09-09 04:04:31 +0530

    removing logic of authenticating a token, as not required in authentication project. The authentication logic is required in other project

--- AuthenticationProject/AuthenticationStartup.cs
+++ AuthenticationProject/AuthenticationStartup.cs
@@ -36,20 +36,6 @@ namespace AuthenticationProject
             services.AddTransient<AuthenticationHelper>();
             services.AddControllers();
 
-            //Jwt Authentication
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = Configuration["Jwt:Issuer"],
-                    ValidAudience = Configuration["Jwt:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-                };
-            });
 
         }
 
